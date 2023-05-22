d113a7f651a9b10174620a6a73a5622c5ad53adf
@hexlet/cli <support@hexlet.io>
2023-05-22 11:40:03 +0500

    submit java-collections-ru/advanced-streams

---
+++ java-collections-ru/advanced-streams/src/main/java/exercise/App.java
@@ -0,0 +1,16 @@
+package exercise;
+
+import java.util.stream.Collectors;
+import java.util.Arrays;
+
+// BEGIN
+class App {
+
+    public static String getForwardedVariables(String cfg) {
+
+
+       return "";
+    }
+
+}
+//END
---
+++ java-collections-ru/advanced-streams/src/test/java/exercise/AppTest.java
@@ -0,0 +1,19 @@
+package exercise;
+
+// import java.util.ArrayList;
+// import java.util.List;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class AppTest {
+
+    private static String data1;
+    private static String data2;
+
+
+}
